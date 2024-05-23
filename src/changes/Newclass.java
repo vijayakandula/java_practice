ef63eb3cf7374921ed21bbbc2f6f7f865f7139da
vijayakandula <kandulavijaya1900@gmail.com>
2024-05-23 12:26:11 +0530

    changes package hasbeen created

---
+++ src/changes/Newclass.java
@@ -0,0 +1,14 @@
+package changes;
+
+
+
+public class Newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("for checking ");
+	}
+	
+
+}
